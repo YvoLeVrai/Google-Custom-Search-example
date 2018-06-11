189cf8be2eb31495ffeb9a2333fb9e75728fad1d
MomoPlay <momocraft49@gmail.com>
2018-06-11 14:14:45 +0200

    Adding comments

--- index.php
+++ index.php
@@ -1,31 +1,28 @@
 <!doctype html>
-<html lang="fr">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Custom image search exemple</title>
 </head>
     <div id="image"></div>
 
     <?php
-        $apiKey = "AIzaSyCIn596Ry0MtSYFdG2DhxHTIBESvIiTVGs";
-        $cx = "000333518738124520541:hhchaiog79u";
-        $search = "rabbit";
-        $url = "https://www.googleapis.com/customsearch/v1?key=".$apiKey."&cx=".$cx."&q=".$search."&callback=hndlr&searchType=image";
+        $apiKey = "AIzaSyCIn596Ry0MtSYFdG2DhxHTIBESvIiTVGs"; // Copy here API key. You can generate yours here https://console.developers.google.com/apis/
+        $browserId = "000333518738124520541:hhchaiog79u"; // Copy here your browser ID. You can create a browser here https://cse.google.fr/cse/all
+        $search = "rabbit"; // Here is your google search
+        $url = "https://www.googleapis.com/customsearch/v1?key=".$apiKey."&cx=".$browserId."&q=".$search."&callback=hndlr&searchType=image";
     ?>
 
-    <script>
+    <script> // This function roll through the results and display them into the image div
         function hndlr(response) {
-            for (var i = 0; i < 1; i++) {
+            for (var i = 0; i < response.items.length; i++) {
                 var item = response.items[i];
                 document.getElementById("image").innerHTML += "<img src=\"" + item.link + "\">";
             }
         }
     </script>
 
-    <script src="<?=$url?>"></script>
+    <script src="<?=$url?>"></script> // Here I run the request
 
 </body>
 </html>
